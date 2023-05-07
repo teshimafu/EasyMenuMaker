b4837c07dcc2a4c9772806354b70a6c9b613bdf9
teshimafu <teshima.fu@gmail.com>
2023-05-07 18:39:59 +0900

    vuetifyのicon設定変更

--- front/components/AppHeader.vue
+++ front/components/AppHeader.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app-bar app color="primary" dark>
-    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-navigation-drawer app>Navigation Lists</v-navigation-drawer>
+    <v-app-bar-nav-icon> </v-app-bar-nav-icon>
     <v-toolbar-title>My App</v-toolbar-title>
-    <label for="name">Name:{{ currentUser.name }}</label>
     <v-spacer></v-spacer>
   </v-app-bar>
 </template>
@@ -11,4 +11,12 @@
 import { useAuth } from '@/composables/useAuth'
 const drawer = ref(false)
 const { currentUser } = useAuth()
+
+const items = reactive([
+  { link: `a`, icon: `b`, title: `c` },
+  { link: `a`, icon: `b`, title: `c` },
+  { link: `a`, icon: `b`, title: `c` },
+  { link: `a`, icon: `b`, title: `c` },
+  { link: `a`, icon: `b`, title: `c` }
+])
 </script>
